c85b04b8b2330f17895653577dd96c586bb40140
iang12 <ianwandersong12@gmail.com>
2019-10-03 18:18:21 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_profile/animations/FadeAnimation.dart';
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  )
+);
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: <Widget>[
+          CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                expandedHeight: 450,
+                backgroundColor: Colors.black,
+                flexibleSpace: FlexibleSpaceBar(
+                  collapseMode: CollapseMode.pin,
+                  background: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/emma.jpg'),
+                        fit: BoxFit.cover
+                      )
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomRight,
+                          colors: [
+                            Colors.black,
+                            Colors.black.withOpacity(.3)
+                          ]
+                        )
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            FadeAnimation(1, Text("Gabriele Silva", style: 
+                              TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 40)
+                            ,)),
+                            SizedBox(height: 20,),
+                            Row(
+                              children: <Widget>[
+                                FadeAnimation(1.2, 
+                                  Text("20 Videos", style: TextStyle(color: Colors.grey, fontSize: 16),)
+                                ),
+                                SizedBox(width: 50,),
+                                FadeAnimation(1.3, Text("40K de Inscritos", style: 
+                                  TextStyle(color: Colors.grey, fontSize: 16)
+                                ,))
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildListDelegate([
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        FadeAnimation(1.6, Text("Gabriele Silva nasceu em Brazi,filha de pais ingleses, Jacqueline Luesby e Chris Watson, ambos advogados. Ela se mudou para Oxfordshire quando tinha cinco anos, onde frequentou a Escola do Dragão.", 
+                        style: TextStyle(color: Colors.grey, height: 1.4),)),
+                        SizedBox(height: 40,),
+                        FadeAnimation(1.6, 
+                          Text("Nascimento", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),)
+                        ),
+                        SizedBox(height: 10,),
+                        FadeAnimation(1.6, 
+                          Text("20 Maio 1994, Brasil, Fortaleza", style: TextStyle(color: Colors.grey),)
+                        ),
+                        SizedBox(height: 20,),
+                        FadeAnimation(1.6, 
+                          Text("Nationalidade", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),)
+                        ),
+                        SizedBox(height: 10,),
+                        FadeAnimation(1.6, 
+                          Text("Brasileira", style: TextStyle(color: Colors.grey),)
+                        ),
+                        SizedBox(height: 20,),
+                        FadeAnimation(1.6, 
+                          Text("Videos", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),)
+                        ),
+                        SizedBox(height: 20,),
+                        FadeAnimation(1.8, Container(
+                          height: 200,
+                          child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            children: <Widget>[
+                              makeVideo(image: 'assets/emma-1.jpg'),
+                              makeVideo(image: 'assets/emma-2.jpg'),
+                              makeVideo(image: 'assets/emma-3.jpg'),
+                            ],
+                          ),
+                        )),
+                        SizedBox(height: 120,)
+                      ],
+                    ),
+                  )
+                ]),
+              )
+            ],
+          ),
+          Positioned.fill(
+            bottom: 50,
+            child: Container(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: FadeAnimation(2,
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 30),
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.yellow[700]
+                    ),
+                    child: Align(child: Text("Seguir", style: TextStyle(color: Colors.white),)),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget makeVideo({image}) {
+    return AspectRatio(
+      aspectRatio: 1.5/ 1,
+      child: Container(
+        margin: EdgeInsets.only(right: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(
+            image: AssetImage(image),
+            fit: BoxFit.cover
+          )
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.bottomRight,
+              colors: [
+                Colors.black.withOpacity(.9),
+                Colors.black.withOpacity(.3)
+              ]
+            )
+          ),
+          child: Align(
+            child: Icon(Icons.play_arrow, color: Colors.white, size: 70,),
+          ),
+        ),
+      ),
+    );
+  }
+}
